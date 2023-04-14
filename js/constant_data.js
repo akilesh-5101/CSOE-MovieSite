c9b1d39e66187b2b8aa8cd869b076b424e308ef5
akilesh-5101 <3011akilesh1510@gmail.com>
2023-04-14 21:25:58 +0530

    First Update with classes messy

---
+++ js/constant_data.js
@@ -0,0 +1,5 @@
+class Elements{
+     constructor(params){   
+       this.data = JSON.parse(JSON.stringify(TV_SHOWS));
+       this.emo = String.fromCodePoint(0x1F604);    
+}
---
+++ js/elements.js
@@ -0,0 +1,5 @@
+class Elements{
+     constructor(params){   
+       
+
+}
